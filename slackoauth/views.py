94322c53b813860cb9012b1bcc378ea11a33cd0f
Josh Fullmer <joshfullmer9@gmail.com>
2018-09-27 17:42:06 -0700

    Remove JSON payload

--- slackoauth/views.py
+++ slackoauth/views.py
@@ -20,7 +20,7 @@ def auth(request):
         base_url = 'https://slack.com/api/oauth.access'
         url = base_url + '?' + data
         print(url)
-        response = requests.post(url, headers=headers, json=data)
+        response = requests.post(url, headers=headers)
         print(response, response.reason)
         r_json = response.json()
         print(r_json)
